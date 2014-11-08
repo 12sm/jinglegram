ec15265561acdf5a7758e7b87aba08d11276621e
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-11-08 12:25:41 -0600

    views work

--- app/app.js
+++ app/app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('home/index.ejs');
 });
 
 app.get('/wtf', function (req, res) {
