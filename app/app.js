907494a8855509dde2e7cebadcb9896de6b63a9f
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-11-08 15:02:23 -0600

    bushit

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@
 var http    = require('http');
 var express = require('express');
 var path    = require('path');
+var mime    = require('mime');
+var mime    = require('fs');
 var app     = express();
 
 
@@ -11,6 +13,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.get('/', function (req, res) {
   res.render('home/index.ejs');
 });
@@ -19,9 +23,9 @@ app.get('/wtf', function (req, res) {
   res.render('wtf');
 });
 
-app.use(function (req,res) {
-  res.render('404', {url:req.url});
-});
+// app.use(function (req,res) {
+//   res.render('404', {url:req.url});
+// });
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
