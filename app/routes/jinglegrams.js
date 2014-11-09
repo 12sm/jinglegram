74677b25d3a72340f4f6ff5210d23ba3293c5821
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-11-09 14:59:36 -0600

    new view

--- app/routes/jinglegrams.js
+++ app/routes/jinglegrams.js
@@ -3,12 +3,6 @@
 var Jinglegram = require('../models/jinglegram.js');
 var jinglegram;
 
-exports.index = function(req, res){
-  Jinglegram.findAll(function(jinglegrams){
-    res.render('jinglegrams/index', {title: 'Showing All Jinglegrams', jinglegrams:jinglegrams});
-  });
-};
-
 exports.create = function(req, res){
   console.log("the form was submitted dummy");
   console.log(req.body);
@@ -18,9 +12,15 @@ exports.create = function(req, res){
   });
 };
 
-// exports.show = function(req, res){
-//   Album.findById(req.params.id, function(album){
-//     res.render('albums/show', {album:album, title:album.title});
+exports.show = function(req, res){
+  Jinglegram.findById(req.params.id, function(album){
+    res.render('albums/show', {album:album, title:album.title});
+  });
+};
+
+// exports.index = function(req, res){
+//   Jinglegram.findAll(function(jinglegrams){
+//     res.render('jinglegrams/index', {title: 'Showing All Jinglegrams', jinglegrams:jinglegrams});
 //   });
 // };
 //
