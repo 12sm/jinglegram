384e099cd4239ec154897cbb4638c7aa19b618e1
michael <michael@12southmusic.com>
2014-11-09 15:21:28 -0600

    Adding Three and Snow

--- app/static/js/source/main-jingle.js
+++ app/static/js/source/main-jingle.js
@@ -1,4 +1,4 @@
-// Plays a jingle on every second beat
+// declare remix vars
 var apiKey = 'TRZJMRXITQGIFF6LK';
 var trackID = 'TRACK-ID-HERE';
 var trackURL = '../../audio/katie.mp3'
@@ -13,8 +13,127 @@ var track;
 var remixed;
 
 
+// delacre snow3d vars
+var SCREEN_WIDTH = window.innerWidth;
+var SCREEN_HEIGHT = window.innerHeight;
 
-function init() {
+var container;
+
+var particle;
+
+var camera;
+var scene;
+var renderer;
+
+var mouseX = 0;
+var mouseY = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+var particles = [];
+var particleImage = new Image();//THREE.ImageUtils.loadTexture( "img/ParticleSmoke.png" );
+particleImage.src = '../../img/ParticlPeSmoke.png';
+
+
+function snow3d() {
+
+  	container = document.createElement('div');
+  	document.body.appendChild(container);
+
+  	camera = new THREE.PerspectiveCamera( 75, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000 );
+  	camera.position.z = 1000;
+
+  	scene = new THREE.Scene();
+  	scene.add(camera);
+
+  	renderer = new THREE.CanvasRenderer();
+  	renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+  	var material = new THREE.ParticleBasicMaterial( { map: new THREE.Texture(particleImage) } );
+
+  	for (var i = 0; i < 500; i++) {
+
+  		particle = new Particle3D( material);
+  		particle.position.x = Math.random() * 2000 - 1000;
+  		particle.position.y = Math.random() * 2000 - 1000;
+  		particle.position.z = Math.random() * 2000 - 1000;
+  		particle.scale.x = particle.scale.y =  1;
+  		scene.add( particle );
+
+  		particles.push(particle);
+  	}
+
+  	container.appendChild( renderer.domElement );
+
+
+  	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+  	document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+  	document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+
+  	setInterval( loop, 1000 / 60 );
+
+  function onDocumentMouseMove( event ) {
+
+  	mouseX = event.clientX - windowHalfX;
+  	mouseY = event.clientY - windowHalfY;
+  }
+
+  function onDocumentTouchStart( event ) {
+
+  	if ( event.touches.length == 1 ) {
+
+  		event.preventDefault();
+
+  		mouseX = event.touches[ 0 ].pageX - windowHalfX;
+  		mouseY = event.touches[ 0 ].pageY - windowHalfY;
+  	}
+  }
+
+  function onDocumentTouchMove( event ) {
+
+  	if ( event.touches.length == 1 ) {
+
+  		event.preventDefault();
+
+  		mouseX = event.touches[ 0 ].pageX - windowHalfX;
+  		mouseY = event.touches[ 0 ].pageY - windowHalfY;
+  	}
+  }
+
+  //
+
+  function loop() {
+
+  for(var i = 0; i<particles.length; i++)
+  	{
+
+  		var particle = particles[i];
+  		particle.updatePhysics();
+
+  		with(particle.position)
+  		{
+  			if(y<-1000) y+=2000;
+  			if(x>1000) x-=2000;
+  			else if(x<-1000) x+=2000;
+  			if(z>1000) z-=2000;
+  			else if(z<-1000) z+=2000;
+  		}
+  	}
+
+  	camera.position.x += ( mouseX - camera.position.x ) * 0.05;
+  	camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
+  	camera.lookAt(scene.position);
+
+  	renderer.render( scene, camera );
+
+
+  }
+}
+
+
+
+
+function jingleRemix() {
     var contextFunction = window.webkitAudioContext || window.AudioContext;
     if (contextFunction === undefined) {
         $("#info").text("Sorry, this app needs advanced web audio. Your browser doesn't"
@@ -60,4 +179,9 @@ function init() {
     }
 }
 
-window.onload = init;
+function jingleInit() {
+  jingleRemix();
+  snow3d();
+}
+
+window.onload = jingleInit;
